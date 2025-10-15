f74f92800c153d57006753fc0c9f27281a880094
MuhNash <nash.collage.life@gmail.com>
2025-10-15 12:00:40 +0700

    v2.0

--- SiakadFor19.java
+++ SiakadFor19.java
@@ -5,10 +5,17 @@ public class SiakadFor19 {
         Scanner input = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidak_lulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("masukkan nilai mahasiswa ke-" + i + " : ");
             nilai = input.nextDouble();
+
+            if (nilai <= 60) {
+                lulus++;
+            } else{
+                tidak_lulus++;
+            }
             if (nilai < terendah) {
                 terendah = nilai;
             }
@@ -18,6 +25,8 @@ public class SiakadFor19 {
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah Lulus: " + lulus);
+        System.out.println("jumlah Tidak Lulus: " + tidak_lulus);
 
         input.close();
     }
